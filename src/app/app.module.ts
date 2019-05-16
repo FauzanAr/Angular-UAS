7ac15090fd2a7a5e308596755fd6142d35d2b3e8
FauzanAr <faurinta12345@gmail.com>
2019-05-16 14:24:55 +0700

    Creating navbar done, creating route done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,29 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FriendComponent } from './friend/friend.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { RouterModule } from '@angular/router';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FriendComponent,
+    ContactUsComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      {path:'home', component:AppComponent},
+      {path:'friend', component:FriendComponent},
+      {path:'contact-us', component:ContactUsComponent},
+      {path:'**', component:NotFoundComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
