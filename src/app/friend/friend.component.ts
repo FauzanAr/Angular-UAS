94f40dbda84959058b74d5ea21a4eb1ef5c9c2fe
FauzanAr <faurinta12345@gmail.com>
2019-05-17 14:46:00 +0700

    Creating form and validators in friend done

--- src/app/friend/friend.component.ts
+++ src/app/friend/friend.component.ts
@@ -1,3 +1,4 @@
+import { FormGroup,FormControl,Validators } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,6 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./friend.component.css']
 })
 export class FriendComponent implements OnInit {
+
+  phoneNumber = "^((\\+62-?)|0)?[0-9]{11}$";
+  form = new FormGroup({
+    name: new FormControl('',[Validators.required, Validators.maxLength(20), Validators.minLength(3)]),
+    email: new FormControl('',[Validators.required, Validators.email]),
+    contact: new FormControl('',[Validators.required, Validators.pattern(this.phoneNumber)])
+  });
+
+
 
   constructor() { }
 
