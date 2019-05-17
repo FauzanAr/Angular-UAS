dd5a343c224eb7d7595cfea2c8a33d60e4cf0729
FauzanAr <faurinta12345@gmail.com>
2019-05-17 22:53:36 +0700

    Login done, except auth guard

--- src/app/verify-email-address/verify-email-address.component.ts
+++ src/app/verify-email-address/verify-email-address.component.ts
@@ -1,3 +1,4 @@
+import { AuthService } from './../shared/services/auth.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VerifyEmailAddressComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    public authService:AuthService
+  ) { }
 
   ngOnInit() {
   }
